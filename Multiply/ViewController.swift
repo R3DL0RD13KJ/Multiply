98c697b9811efd7841a85325c6825aec9df2e320
Kenneth Johnson <kd123johnson@gmail.com>
2021-09-27 10:55:02 -0600

    Multiply 1

--- Multiply/ViewController.swift
+++ Multiply/ViewController.swift
@@ -7,13 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    
+    //Variables
+    
+    @IBOutlet weak var thingOne: UITextField!
+    
+    @IBOutlet weak var thingTwo: UITextField!
+    
+    @IBOutlet weak var thingThree: UILabel!
+    
+    @IBOutlet weak var thingFour: UILabel!
+    
+   
+    
+    
+    //FunctionTIME
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+        //Actions
+    
+    
+    @IBAction func pizzaTime(_ sender: Any)
+    {
+        var thingOne = thingOne.text ?? "1"
+        var thingTwo = thingTwo.text ?? "1"
+        
+        var appleDeletus = Int(thingOne) ?? 1
+        var orangePower = Int(thingTwo) ?? 1
+        
+        var doubleDeckerHamburger = appleDeletus * orangePower
+        thingFour.text = "\(doubleDeckerHamburger)"
+        
+    }
+    
+    
 
 }
 
